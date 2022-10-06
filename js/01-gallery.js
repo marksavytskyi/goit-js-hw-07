7249c4c27e2e97d8d5b3172b6f2b64aee573695b
marksavytskyi <dorozhe.zolota777@gmail.com>
2022-10-06 12:23:44 +0300

    do it

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,63 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refGallery = document.querySelector('.gallery');
+
+refGallery.addEventListener('click', onGetedUrlBigImg)
+
+const innerHTML = renderGalleryItems(galleryItems)
+
+refGallery.innerHTML = innerHTML;
+
+
+function renderGalleryItems(items) {
+    return   items.map(el => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>
+</div>`
+    }).join('')
+}
+
+function onGetedUrlBigImg(e) {
+  e.preventDefault()
+  const imgBigSize = e.target.dataset.source
+  
+  if (e.target.nodeName !== 'IMG') {
+    return
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${imgBigSize}" width="800" height="600">
+  `)
+  instance.show()
+
+  const visible = basicLightbox.visible()
+ 
+
+  if (visible) {
+    window.addEventListener('keydown', onKeyPresEskToCloseModal)
+
+    function onKeyPresEskToCloseModal(e) {
+      const keyEscape = e.code === `Escape`
+
+      if (keyEscape) {
+        instance.close()
+      }
+    }
+  }
+
+  }
+
+
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+
+
+// Change code below this line
 console.log(galleryItems);
+const refGallery = document.querySelector('.gallery');
+const innerHTML = renderGalleryItems(galleryItems)
+
+refGallery.innerHTML = innerHTML;
+
+
+function renderGalleryItems(items) {
+    return   items.map(el => {
+        return `<a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>`
+    }).join('')
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: `alt`,
+    captionDelay: 250
+  });
+console.log(lightbox);
+
+
+
